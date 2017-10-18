10c8c364b31258659fe1699591bd1fd74b192fda
Hailey Melville <haileykmelville@gmail.com>
2017-10-18 17:16:32 +0000

    added google maps

--- models/campground.js
+++ models/campground.js
@@ -2,9 +2,12 @@ var mongoose = require("mongoose")
 
 var campgroundScehema = new mongoose.Schema({
     name: String,
-    price: String,
     image: String,
     description: String,
+    price: Number,
+    location: String,
+    lat: Number, 
+    lng: Number,
     author: {
         id: {
             type: mongoose.Schema.Types.ObjectId,
